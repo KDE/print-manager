5233fa435548ba35550bcd4f052b463daba35e16
Kai Uwe Broulik <kde@privat.broulik.de>
2012-06-02 22:49:49 +0200

    Make Printer Manager KCM translateable in a different po file and disable root Messages.sh I will remove it once all the changes are completed. For now it only creates the po files for the plasmoid which will probably stay this way?

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.rc" -o -name "*.ui"` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/print-manager.pot
+# $EXTRACTRC `find . -name "*.rc" -o -name "*.ui"` >> rc.cpp
+# $XGETTEXT `find . -name \*.cpp` -o $podir/print-manager.pot
 $XGETTEXT `find . -name \*.qml` -o $podir/plasma_applet_printmanager.pot
-rm -f rc.cpp
+# rm -f rc.cpp
 

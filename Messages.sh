fdf89f50f22a792619b9b71fb902fd9096787c19
Lukáš Tinkl <lukas@kde.org>
2013-05-07 17:47:58 +0200

    fix the messages target here as well

--- Messages.sh
+++ Messages.sh
@@ -3,4 +3,4 @@ $EXTRACTRC `find . -name "*.rc" -o -name "*.ui"` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp` -o $podir/print-manager.pot
 rm -f rc.cpp
 
-$XGETTEXT `find . -name \*.qml` -o $podir/plasma_package_printmanager.pot
+$XGETTEXT `find plasmoid -name \*.qml` -L Java -o $podir/plasma_applet_printmanager.pot

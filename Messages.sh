adf0385df3cff9cc164acf91f4c1f0c2d05f993c
Lukáš Tinkl <lukas@kde.org>
2013-05-07 17:51:12 +0200

    also extract the CPP files for the plasmoid

--- Messages.sh
+++ Messages.sh
@@ -3,4 +3,5 @@ $EXTRACTRC `find . -name "*.rc" -o -name "*.ui"` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp` -o $podir/print-manager.pot
 rm -f rc.cpp
 
-$XGETTEXT `find plasmoid -name \*.qml` -L Java -o $podir/plasma_applet_printmanager.pot
+$XGETTEXT `find plasmoid -name \*.cpp` -o $podir/plasma_applet_printmanager.pot
+$XGETTEXT `find plasmoid -name \*.qml` -j -L Java -o $podir/plasma_applet_printmanager.pot

ed45d4dbdb2254cdc0ea41b13a91798e130d67f7
Lukáš Tinkl <lukas@kde.org>
2013-05-21 20:36:47 +0200

    get both the lines correct

--- Messages.sh
+++ Messages.sh
@@ -3,7 +3,7 @@ $EXTRACTRC `find . -name "*.rc" -o -name "*.ui" | grep -v "plasmoid"` >> rc.cpp
 $XGETTEXT `find . -name "*.cpp" | grep -v "plasmoid"` -o $podir/print-manager.pot
 rm -f rc.cpp
 
-$XGETTEXT `find plasmoid -name "*.cpp" -o -name "*.ui"` -o $podir/plasma_applet_printmanager.pot
+$XGETTEXT `find plasmoid -name "*.cpp" -o -name "*.ui"` -o $podir/plasma_applet_org.kde.printmanager.pot
 # At the moment there's nothing in the .cpp so just make sure to touch
 # the .pot so that -j doesn't fail
 touch $podir/plasma_applet_printmanager.pot

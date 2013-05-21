f4104d5391e55fe87095f8d64eaa4aa0de5ed4c6
Albert Astals Cid <aacid@kde.org>
2013-05-21 20:18:51 +0200

    Make Messages.sh work
    Are you sure the name of the .pot is the correct one?
    
    CCMAIL: lukas@kde.org

--- Messages.sh
+++ Messages.sh
@@ -4,4 +4,7 @@ $XGETTEXT `find . -name \*.cpp` -o $podir/print-manager.pot
 rm -f rc.cpp
 
 $XGETTEXT `find plasmoid -name \*.cpp` -o $podir/plasma_applet_printmanager.pot
+# At the moment there's nothing in the .cpp so just make sure to touch
+# the .pot so that -j doesn't fail
+touch $podir/plasma_applet_printmanager.pot
 $XGETTEXT `find plasmoid -name \*.qml` -j -L Java -o $podir/plasma_applet_printmanager.pot

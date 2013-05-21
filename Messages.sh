63d9e62b97d93fe6e4c64994bdf171a114d8d18f
Albert Astals Cid <aacid@kde.org>
2013-05-21 20:52:35 +0200

    Fix ui extraction

--- Messages.sh
+++ Messages.sh
@@ -3,8 +3,6 @@ $EXTRACTRC `find . -name "*.rc" -o -name "*.ui" | grep -v "plasmoid"` >> rc.cpp
 $XGETTEXT `find . -name "*.cpp" | grep -v "plasmoid"` -o $podir/print-manager.pot
 rm -f rc.cpp
 
-$XGETTEXT `find plasmoid -name "*.cpp" -o -name "*.ui"` -o $podir/plasma_applet_org.kde.printmanager.pot
-# At the moment there's nothing in the .cpp so just make sure to touch
-# the .pot so that -j doesn't fail
-touch $podir/plasma_applet_printmanager.pot
+$EXTRACTRC `find plasmoid -name "*.rc" -o -name "*.ui"` >> rc.cpp
+$XGETTEXT rc.cpp `find plasmoid -name "*.cpp"` -o $podir/plasma_applet_org.kde.printmanager.pot
 $XGETTEXT `find plasmoid -name "*.qml"` -j -L Java -o $podir/plasma_applet_org.kde.printmanager.pot

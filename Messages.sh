5858a0e69e9dd31e598ea151d7ebf3b3c7fdcdf3
Lukáš Tinkl <lukas@kde.org>
2013-05-21 20:32:28 +0200

    correct the catalog name of the QML plasmoid
    exclude the plasmoid strings from the global print-manager catalog

--- Messages.sh
+++ Messages.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.rc" -o -name "*.ui"` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/print-manager.pot
+$EXTRACTRC `find . -name "*.rc" -o -name "*.ui" | grep -v "plasmoid"` >> rc.cpp
+$XGETTEXT `find . -name "*.cpp" | grep -v "plasmoid"` -o $podir/print-manager.pot
 rm -f rc.cpp
 
-$XGETTEXT `find plasmoid -name \*.cpp` -o $podir/plasma_applet_printmanager.pot
+$XGETTEXT `find plasmoid -name "*.cpp" -o -name "*.ui"` -o $podir/plasma_applet_printmanager.pot
 # At the moment there's nothing in the .cpp so just make sure to touch
 # the .pot so that -j doesn't fail
 touch $podir/plasma_applet_printmanager.pot
-$XGETTEXT `find plasmoid -name \*.qml` -j -L Java -o $podir/plasma_applet_printmanager.pot
+$XGETTEXT `find plasmoid -name "*.qml"` -j -L Java -o $podir/plasma_applet_org.kde.printmanager.pot

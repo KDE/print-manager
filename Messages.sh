071739a3d68d02174f3a6e665db69f12412fbce5
Albert Astals Cid <aacid@kde.org>
2015-02-18 21:11:03 +0100

    Fix Messages.sh
    Seems plasmoid doesn't cpp nor ui anymore
    
    CCMAIL: wbauer@tmo.at

--- Messages.sh
+++ Messages.sh
@@ -3,6 +3,4 @@ $EXTRACTRC `find . -name "*.rc" -o -name "*.ui" | grep -v "plasmoid"` >> rc.cpp
 $XGETTEXT `find . -name "*.cpp" | grep -v "plasmoid"` -o $podir/print-manager.pot
 rm -f rc.cpp
 
-$EXTRACTRC `find plasmoid -name "*.rc" -o -name "*.ui"` >> rc.cpp
-$XGETTEXT rc.cpp `find plasmoid -name "*.cpp"` -o $podir/plasma_applet_org.kde.plasma.printmanager.pot
-$XGETTEXT `find plasmoid -name "*.qml"` -j -L Java -o $podir/plasma_applet_org.kde.plasma.printmanager.pot
+$XGETTEXT `find plasmoid -name "*.qml"` -L Java -o $podir/plasma_applet_org.kde.plasma.printmanager.pot

KDE/print-manager

commit 2b80c63fa29a6ec1201356070c1a531c1ea241ed

Make calling configure-printer from kded work the same way as it works in KCM

print-manager-kded/NewPrinterNotification.cpp
@@ -229,14 +229,7 @@ bool NewPrinterNotification::registerService()
 
 void NewPrinterNotification::configurePrinter()
 {
-    QDBusMessage message;
-    message = QDBusMessage::createMethodCall(QLatin1String("org.kde.ConfigurePrinter"),
-                                             QLatin1String("/"),
-                                             QLatin1String("org.kde.ConfigurePrinter"),
-                                             QLatin1String("ConfigurePrinter"));
-    // TODO setup wid
-    message << sender()->property(PRINTER_NAME);
-    QDBusConnection::sessionBus().send(message);
+    QProcess::startDetached("configure-printer", {PRINTER_NAME});
 }
 
 void NewPrinterNotification::searchDrivers()

commit 74cc553cd82278542b0e27c742c8eaf0d39e7f19

Fix action to configure newly added printer broken by commit 2b80c63fa29a6ec1201356070c1a531c1ea241ed

print-manager-kded/NewPrinterNotification.cpp
@@ -145,7 +145,7 @@ bool NewPrinterNotification::registerService()
 
 void NewPrinterNotification::configurePrinter()
 {
-    QProcess::startDetached(QLatin1String("configure-printer"), { QLatin1String(PRINTER_NAME) });
+    QProcess::startDetached(QLatin1String("configure-printer"), { sender()->property(PRINTER_NAME).toString() });
 }
 
 void NewPrinterNotification::searchDrivers()
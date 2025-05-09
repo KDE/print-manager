98f2e5019ecd0d5b86f7ccd3437674915695dfa5
Mike Noe <noeerover@gmail.com>
2025-05-09 15:54:28 -0400

    kded: Add marker levels checker class
    Listen for printer state change and job creation/completion
    to check marker levels and create a notification event
    for the marker levels.
    
    Notify if any marker level is at or below threshold
    (3%)

---
+++ src/kded/MarkerLevelChecker.h
@@ -0,0 +1,64 @@
+/*
+    SPDX-FileCopyrightText: 2025 Mike Noe <noeerover@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+#include <QPointer>
+
+/**
+ * Check for at least one marker level being below the threshold.
+ *
+ * CUPS only updates printers.conf after the first print
+ * job is created/sent, so marker-* attributes will not exist until that point.
+ * For a newly added printer queue or discovered printer, this will be a noop.
+ *
+ * Assumes levels and thresholds are percentages.
+ * see https://openprinting.github.io/cups/doc/spec-ipp.html#marker-high-levels
+ *
+ * Maintains a \sa KCupsConnection for the lifetime.
+ */
+class KCupsConnection;
+class MarkerLevelChecker : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit MarkerLevelChecker(QObject *parent);
+    ~MarkerLevelChecker() = default;
+
+private:
+    QPointer<KCupsConnection> m_connection;
+
+    void checkMarkerLevels(const QString &printerName);
+
+    void init();
+    /**
+     * @brief Force a new cups connection.  When the cups scheduler
+     * stops the subscriptions for notify appear to be in a unknown
+     * state - sometimes working, sometimes not.
+     *
+     * Forcing a new connection guarantees valid subscriptions and
+     * prevents the checker from errantly issuing a request on a
+     * potentially stale connection should the CUPS scheduler be
+     * stopped.
+     */
+    void reset();
+    /**
+     * @brief Generic handler for job signals
+     */
+    void jobHandler(const QString &text,
+                    const QString &printerUri,
+                    const QString &printerName,
+                    uint printerState,
+                    const QString &printerStateReasons,
+                    bool printerIsAcceptingJobs,
+                    uint jobId,
+                    uint jobState,
+                    const QString &jobStateReasons,
+                    const QString &jobName,
+                    uint jobImpressionsCompleted);
+};

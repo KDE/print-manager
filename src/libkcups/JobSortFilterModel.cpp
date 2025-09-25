82dd7756c50faaa3c42ceab08ef1fd9118b04344
Mike Noe <noeerover@gmail.com>
2025-09-25 14:03:02 -0400

    libkcups: Update jobs model for new print queue manager
    - added roles
    - remove drag/drop QWidget overrides (handled by apps)
    - remove per-printer filter  (handled by QSFPM)
    - port processing msgs to a list and make Q_PROPERTY
    - make which-jobs-filter Q_PROPERTY
    - other clean up

--- src/libkcups/JobSortFilterModel.cpp
+++ src/libkcups/JobSortFilterModel.cpp
@@ -5,9 +5,7 @@
 */
 
 #include "JobSortFilterModel.h"
-
 #include "JobModel.h"
-#include "kcupslib_log.h"
 
 JobSortFilterModel::JobSortFilterModel(QObject *parent)
     : QSortFilterProxyModel(parent)
@@ -38,12 +36,13 @@ void JobSortFilterModel::setModel(QAbstractItemModel *model)
 
 void JobSortFilterModel::setFilteredPrinters(const QString &printers)
 {
+    beginFilterChange();
     if (printers.isEmpty()) {
         m_filteredPrinters.clear();
     } else {
         m_filteredPrinters = printers.split(QLatin1Char('|'));
     }
-    invalidateFilter();
+    endFilterChange(QSortFilterProxyModel::Direction::Rows);
     Q_EMIT filteredPrintersChanged();
 }
 
@@ -71,10 +70,10 @@ int JobSortFilterModel::count() const
 
 bool JobSortFilterModel::filterAcceptsRow(int source_row, const QModelIndex &source_parent) const
 {
-    QModelIndex index = sourceModel()->index(source_row, 0, source_parent);
 
     // check if the printer is on the blacklist
     if (!m_filteredPrinters.isEmpty()) {
+        const auto index = sourceModel()->index(source_row, 0, source_parent);
         return m_filteredPrinters.contains(index.data(JobModel::RoleJobPrinter).toString());
     }
 

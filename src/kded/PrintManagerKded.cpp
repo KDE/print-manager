e31d3d7557b62af5ac70b9972dc0ced998809179
Mike Noe <noeerover@gmail.com>
2026-02-11 09:02:05 -0500

    libkcups/kded: Put CUPS2-only features behind #ifdefs

--- src/kded/PrintManagerKded.cpp
+++ src/kded/PrintManagerKded.cpp
@@ -1,12 +1,15 @@
 /*
     SPDX-FileCopyrightText: 2010-2018 Daniel Nicoletti <dantti12@gmail.com>
+    SPDX-FileCopyrightText: 2026 Mike Noe <noeerover@gmail.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 #include "PrintManagerKded.h"
 #include "MarkerLevelChecker.h"
+#ifdef LIBCUPS_VERSION_2
 #include "NewPrinterNotification.h"
+#endif
 #include <KPluginFactory>
 
 K_PLUGIN_CLASS_WITH_JSON(PrintManagerKded, "printmanager.json")
@@ -16,7 +19,9 @@ PrintManagerKded::PrintManagerKded(QObject *parent, const QVariantList &args)
 {
     Q_UNUSED(args)
 
+#ifdef LIBCUPS_VERSION_2
     new NewPrinterNotification(this);
+#endif
     new MarkerLevelChecker(this);
 }
 

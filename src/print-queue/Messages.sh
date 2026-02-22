1f1e9e66c17f2f5ecf7e406eeeecfd9e6d99953b
Mike Noe <noeerover@gmail.com>
2026-02-22 09:08:09 -0500

    print-queue: Fix comment in Messages.sh

--- src/print-queue/Messages.sh
+++ src/print-queue/Messages.sh
@@ -1,8 +1,6 @@
-/*
- *   SPDX-FileCopyrightText: 2026 Mike Noe <noeerover@gmail.com>
- *   SPDX-License-Identifier: LGPL-2.0-or-later
- */
-
 #!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2026 Mike Noe <noeerover@gmail.com>
+# SPDX-License-Identifier: LGPL-2.0-or-later
 
 $XGETTEXT `find . -name '*.cpp' -o -name '*.qml'` -o $podir/org.kde.plasma.printqueue.pot

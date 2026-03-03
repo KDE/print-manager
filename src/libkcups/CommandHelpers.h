9a673c2d14e82b17d2c576d86c59656f9dc0b193
Mike Noe <noeerover@gmail.com>
2026-03-02 19:40:39 -0500

    libkcups: Add CommandHelpers class
    - Consolidate printer commands into a single class available
    to all consumer types: QML as well as C++ instance
    
    - Isolate consumers from the details of the KCupsRequest
    but still provides notifications for each command
    
    - Sets the groundwork to allow the creation of units test
    using the single source of printer commands to script CUPS transactions
    
    PrinterModel: Remove printer-specific commands and helpers

---
+++ src/libkcups/CommandHelpers.h
@@ -0,0 +1,75 @@
+/*
+    SPDX-FileCopyrightText: 2026 Mike Noe <noeerover@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+#include <kcups_export.h>
+#include <qqmlregistration.h>
+
+class KCupsRequest;
+
+class KCUPS_EXPORT PrinterCommands : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+
+public:
+    /**
+     * @brief Referencing the Qml Singleton and C++ global in the same
+     * process will create two distinct instances.
+     * @return instance
+     */
+    static PrinterCommands *instance();
+
+    explicit PrinterCommands(QObject *parent = nullptr);
+    ~PrinterCommands() override;
+
+#ifdef LIBCUPS_VERSION_2
+    enum PPDType {
+        Manual = 0,
+        Auto,
+        Custom
+    };
+    Q_ENUM(PPDType)
+#endif
+    Q_INVOKABLE void pausePrinter(const QString &printerName);
+    Q_INVOKABLE void resumePrinter(const QString &printerName);
+    Q_INVOKABLE void removePrinter(const QString &printerName);
+
+    Q_INVOKABLE void setDefault(const QString &printerName);
+    Q_INVOKABLE void setShared(const QString &printerName, bool isClass, bool shared = true);
+    Q_INVOKABLE void setAcceptingJobs(const QString &printerName, bool accept = true);
+
+    Q_INVOKABLE void printTestPage(const QString &printerName, bool isClass);
+    Q_INVOKABLE void printSelfTestPage(const QString &printerName);
+    Q_INVOKABLE void cleanPrintHeads(const QString &printerName);
+
+    Q_INVOKABLE void savePrinter(const QString &printerName, const QVariantMap &saveArgs, bool isClass);
+
+Q_SIGNALS:
+    void error(int lastError, const QString &errorTitle, const QString &errorMsg);
+    void removeDone();
+    void saveDone(bool forceRefresh);
+    void pauseDone();
+    void resumeDone();
+    void defaultDone();
+    void sharedDone();
+    void testDone();
+    void cleanDone();
+    void acceptDone();
+
+private:
+    using SignalFunc = void (PrinterCommands::*)();
+    using RequestFunc = void (PrinterCommands::*)(KCupsRequest *);
+    using StdRequestFunc = std::function<void(KCupsRequest *)>;
+
+    void defaultErrorCB(KCupsRequest *request);
+    KCupsRequest *setupRequest(SignalFunc finished_cb, RequestFunc error_cb = nullptr);
+    KCupsRequest *setupRequest(RequestFunc finished_cb, RequestFunc error_cb = nullptr);
+    KCupsRequest *setupRequest(StdRequestFunc finished_cb, StdRequestFunc error_cb = nullptr);
+};
